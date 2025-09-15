6703cf3cc6ef56249f1b1faf19059a0b337b2477
AnonoyStar-Coder <anonystargamerz@gmail.com>
2025-09-15 09:15:47 +0530

    update: stack.cpp updated

--- GNDEC- DCLD/stack.cpp
+++ GNDEC- DCLD/stack.cpp
@@ -1,2 +1,44 @@
 #include<iostream>
 using namespace std;
+
+int stack[MAX];
+int top = -1;
+
+void push(int val) {
+    if (top >= MAX - 1) {
+        cout << "Stack Overflow" << endl;
+    } else {
+        top++;
+        stack[top] = val;
+    }
+}
+
+void pop() {
+    if (top <= -1) {
+        cout << "Stack Underflow" << endl;
+    } else {
+        cout << "The popped element is " << stack[top] << endl;
+        top--;
+    }
+}
+
+void display() {
+    if (top >= 0) {
+        cout << "Stack elements are:";
+        for (int i = top; i >= 0; i--)
+            cout << stack[i] << " ";
+        cout << endl;
+    } else {
+        cout << "Stack is empty" << endl;
+    }
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    display();
+    pop();
+    display();
+    return 0;
+}
